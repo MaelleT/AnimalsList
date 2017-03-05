325b11b98ed1a04cd75dfce61b9374ebde3e4d6f
MaelleT <maelle.taurand@ac-nantes.fr>
2017-03-05 16:04:27 +0100

    Add one animal

--- animals.py
+++ animals.py
@@ -3,7 +3,7 @@ Decouverte Application Rest
 
 @author: maelle
 '''
-from bottle import run,get
+from bottle import run,get,post,request
 
 animals = [{'name' : 'Dumbo','type':'elephant'},
            {'name' : 'Python','type':'serpent'},
@@ -18,8 +18,14 @@ def getAll():
 def getOne(name):
      
     the_animal = [animal for animal in animals if animal['name']==name]
-
+    
     return {'animal' : the_animal[0]}
+
+@post('/animal')
+def addOne():
+    new_animal = {'name': request.json.get('name'), 'type':request.json.get('type')}
+    animals.append(new_animal)
+    return {'animals':animals}
 
 
 run(reloader=True,debug=True)
